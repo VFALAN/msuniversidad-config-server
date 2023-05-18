f3d2fc48622c53263faf41440a4077a89668d9e0
VFALAN <alanvillafan95@gmail.com>
2023-05-17 20:45:22 -0600

    Commit inicial

---
+++ src/test/java/com/vf/dev/msuniversidadconfigserver/MsuniversidadConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vf.dev.msuniversidadconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsuniversidadConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
